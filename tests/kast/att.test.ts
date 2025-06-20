d343052cf9b75e6d3aa2bc431d1ec1dd9fd36430
Yiyi Wang <yiyi.wang@runtimeverification.com>
2025-06-20 10:47:42 +0800

    feat: Add tests for att.test.ts

---
+++ tests/kast/att.test.ts
@@ -0,0 +1,36 @@
+import { describe, expect, test } from "bun:test";
+import type { AttEntry } from "../../src/kast/att";
+import { Atts, KAtt } from "../../src/kast/att";
+
+const PRETTY_TEST_DATA: Array<[string, AttEntry[], string]> = [
+  ["empty", [], ""],
+  ["nullary", [Atts.FUNCTION.call(null)], "[function]"],
+  [
+    "two-nullaries",
+    [Atts.FUNCTION.call(null), Atts.TOTAL.call(null)],
+    "[function, total]",
+  ],
+  ["opt-none", [Atts.CONCRETE.call(null)], "[concrete]"],
+  ["opt-some-empty-str", [Atts.CONCRETE.call("")], '[concrete("")]'],
+  ["opt-some-nonempty-str", [Atts.CONCRETE.call("foo")], '[concrete("foo")]'],
+  [
+    "multiple",
+    [Atts.SYMBOL.call("foo"), Atts.FUNCTION.call(null), Atts.TOTAL.call(null)],
+    '[symbol("foo"), function, total]',
+  ],
+];
+
+describe("KAtt", () => {
+  PRETTY_TEST_DATA.forEach(([testId, entries, expected]) => {
+    test(`pretty: ${testId}`, () => {
+      // Given
+      const att = new KAtt(entries);
+
+      // When
+      const actual = att.pretty;
+
+      // Then
+      expect(actual).toBe(expected);
+    });
+  });
+});

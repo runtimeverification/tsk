adfcdcec66603fa147ef04634cc60a721d717064
Yiyi Wang <yiyi.wang@runtimeverification.com>
2025-07-02 17:58:53 +0800

    fix: Export Formatter and PrettyPrinter

--- src/kast/index.ts
+++ src/kast/index.ts
@@ -1,7 +1,9 @@
 export { astToKast } from "./_ast_to_kast";
 export { AttEntry, AttKey, Atts, EMPTY_ATT, KAtt } from "./att";
 export type { WithKAtt } from "./att";
+export { Formatter } from "./formatter";
 export { KInner } from "./inner";
 export { KAst, kastTerm } from "./kast";
 export { buildClaim, buildRule } from "./manip";
 export { KOuter } from "./outer";
+export { PrettyPrinter } from "./pretty";

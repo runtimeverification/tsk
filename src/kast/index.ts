355a4ac3402ec0bbb8a07e2ae319ed340200f48e
Yiyi Wang <101185369+yiyi-wang-rv@users.noreply.github.com>
2025-07-08 12:52:17 +0800

    fix: Update more members (#4)

--- src/kast/index.ts
+++ src/kast/index.ts
@@ -1,9 +1,27 @@
 export { astToKast } from "./_ast_to_kast";
-export { AttEntry, AttKey, Atts, EMPTY_ATT, KAtt } from "./att";
+export {
+  AttEntry,
+  AttKey,
+  Atts,
+  EMPTY_ATT,
+  Format,
+  FormatType,
+  KAtt,
+} from "./att";
 export type { WithKAtt } from "./att";
 export { Formatter } from "./formatter";
-export { KInner } from "./inner";
+export {
+  KApply,
+  KAs,
+  KInner,
+  KLabel,
+  KRewrite,
+  KSequence,
+  KSort,
+  KToken,
+  KVariable,
+} from "./inner";
 export { KAst, kastTerm } from "./kast";
 export { buildClaim, buildRule } from "./manip";
-export { KOuter } from "./outer";
+export * from "./outer";
 export { PrettyPrinter } from "./pretty";

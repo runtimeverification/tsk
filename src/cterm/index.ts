d3b54f3e6b2d5e565dfbf23a566cf49cca3fa844
Yiyi Wang <yiyi.wang@runtimeverification.com>
2025-07-14 16:48:41 +0800

    fix: Fix export from cterm

--- src/cterm/index.ts
+++ src/cterm/index.ts
@@ -1,6 +1,2 @@
 export { CSubst, CTerm, ctermBuildClaim, ctermBuildRule } from "./cterm";
-export {
-  createAsyncCTermShow,
-  createUltraSafeCTermShow,
-  CTermShow,
-} from "./show";
+export { createIterativeCTermShow, CTermShow } from "./show";
